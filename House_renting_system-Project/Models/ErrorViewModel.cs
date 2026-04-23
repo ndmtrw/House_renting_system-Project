67179d7215b751bbd8c491a387702079cf8876ce
ndmtrw <atanas.a.dimitrov.highschool@buditel.bg>
2026-04-23 11:26:40 +0300

    Delete House_renting_system-Project directory

--- House_renting_system-Project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace House_renting_system_Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
